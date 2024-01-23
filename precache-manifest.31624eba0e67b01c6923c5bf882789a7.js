0763db7cfd149b84a34849bde59907ed0a448992
eag1ex <eaglex@eaglex.net>
2024-01-23 15:52:13 +0700

    Updates

---
+++ precache-manifest.31624eba0e67b01c6923c5bf882789a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a671794491f5137081c4cae50ce140ac",
+    "url": "https://eag1ex.github.io/bucket-list-app-client/index.html"
+  },
+  {
+    "revision": "fd3c174ebd43b9ae99ce",
+    "url": "https://eag1ex.github.io/bucket-list-app-client/static/css/main.041d44e9.chunk.css"
+  },
+  {
+    "revision": "e275c94533a90adbea7c",
+    "url": "https://eag1ex.github.io/bucket-list-app-client/static/js/2.86668f14.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "https://eag1ex.github.io/bucket-list-app-client/static/js/2.86668f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3c174ebd43b9ae99ce",
+    "url": "https://eag1ex.github.io/bucket-list-app-client/static/js/main.3f7eb3c4.chunk.js"
+  },
+  {
+    "revision": "169487e1529f25261ade",
+    "url": "https://eag1ex.github.io/bucket-list-app-client/static/js/runtime-main.2674a38b.js"
+  }
+]);
